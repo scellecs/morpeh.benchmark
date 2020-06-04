2d460b5e5013a5e8be0482fe7e6f68f5ab99d992
Oleg Morozov <benjminmoore@gmail.com>
2020-06-04 17:42:19 +0300

    Refactor Morpeh Bench

--- Assets/Benchmark/Morpeh/BenchComponent.cs
+++ Assets/Benchmark/Morpeh/BenchComponent.cs
@@ -1,11 +1,13 @@
 ﻿using Morpeh;
 using UnityEngine;
 using Unity.IL2CPP.CompilerServices;
+using Unity.Mathematics;
 
 [Il2CppSetOption(Option.NullChecks, false)]
 [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
 [Il2CppSetOption(Option.DivideByZeroChecks, false)]
 [System.Serializable]
 public struct BenchComponent : IComponent {
+    public float3 position;
     public Transform transform;
 }
--- Assets/Benchmark/Morpeh/BenchSystem.cs
+++ Assets/Benchmark/Morpeh/BenchSystem.cs
@@ -1,6 +1,9 @@
-﻿using Morpeh;
+﻿using System.Runtime.CompilerServices;
+using Morpeh;
 using UnityEngine;
 using Unity.IL2CPP.CompilerServices;
+using Unity.Mathematics;
+using Random = UnityEngine.Random;
 
 [Il2CppSetOption(Option.NullChecks, false)]
 [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
@@ -8,14 +11,20 @@ using Unity.IL2CPP.CompilerServices;
 [CreateAssetMenu(menuName = "ECS/Systems/" + nameof(BenchSystem))]
 public sealed class BenchSystem : UpdateSystem {
     public GameObject prefab;
-    public Vector3    moveVector = Vector3.forward;
+    public float3 moveVector = new float3(0f, 0f, 1f);
 
     private Filter filter;
     
     public override void OnAwake() {
         for (int i = 0, length = CountInput.CountEntities; i < length; i++) {
-            var pos = new Vector3(Random.Range(-100f, 100f), Random.Range(-100f, 100f), Random.Range(-100f, 100f));
-            Instantiate(this.prefab, pos, Quaternion.identity);
+            var entity = this.World.CreateEntity();
+            var pos = new float3(Random.Range(-100f, 100f), Random.Range(-100f, 100f), Random.Range(-100f, 100f));
+            var instance = Instantiate(this.prefab, pos.ToVector3(), Quaternion.identity);
+            
+            entity.SetComponent(new BenchComponent {
+                position = pos,
+                transform = instance.transform
+            });
         }
 
         this.filter = this.World.Filter.With<BenchComponent>();
@@ -26,7 +35,22 @@ public sealed class BenchSystem : UpdateSystem {
 
         for (int i = 0, length = this.filter.Length; i < length; i++) {
             ref var bench = ref posBag.GetComponent(i);
-            bench.transform.position += this.moveVector;
+            bench.position += this.moveVector;
+            bench.transform.position = bench.position.ToVector3();
         }
+    }
+}
+
+[Il2CppSetOption(Option.NullChecks, false)]
+[Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+[Il2CppSetOption(Option.DivideByZeroChecks, false)]
+public static class Float3Extensions {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3 ToVector3(this float3 f3) {
+        Vector3 v;
+        v.x = f3.x;
+        v.y = f3.y;
+        v.z = f3.z;
+        return v;
     }
 }
