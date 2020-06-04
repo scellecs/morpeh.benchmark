d569cd4572aabe183e86eba6a65724e3a16be1fa
Oleg Morozov <benjminmoore@gmail.com>
2020-06-04 17:45:50 +0300

    Refactor Morpeh Job Bench

--- Assets/Benchmark/Morpeh/BenchJobSystem.cs
+++ Assets/Benchmark/Morpeh/BenchJobSystem.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using Unity.IL2CPP.CompilerServices;
 using Unity.Jobs;
 using UnityEngine.Jobs;
+using Random = UnityEngine.Random;
 
 [Il2CppSetOption(Option.NullChecks, false)]
 [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
@@ -12,24 +13,17 @@ using UnityEngine.Jobs;
 [CreateAssetMenu(menuName = "ECS/Systems/" + nameof(BenchJobSystem))]
 public sealed class BenchJobSystem : UpdateSystem {
     public GameObject prefab;
-    public Vector3 moveVector = Vector3.forward;
+    public Vector3 moveVector = new Vector3(0f, 0f, 1f);
     private TransformAccessArray transforms;
 
     private Filter filter;
     
     public override void OnAwake() {
+        this.transforms = new TransformAccessArray(CountInput.CountEntities);
         for (int i = 0, length = CountInput.CountEntities; i < length; i++) {
             var pos = new Vector3(Random.Range(-100f, 100f), Random.Range(-100f, 100f), Random.Range(-100f, 100f));
-            Instantiate(this.prefab, pos, Quaternion.identity);
-        }
-
-        this.filter = this.World.Filter.With<BenchComponent>();
-        this.World.UpdateFilters();
-        
-        this.transforms = new TransformAccessArray(this.filter.Length);
-        foreach (var entity in this.filter) {
-            ref var bench = ref entity.GetComponent<BenchComponent>();
-            this.transforms.Add(bench.transform);
+            var instance = Instantiate(this.prefab, pos, Quaternion.identity);
+            this.transforms.Add(instance.transform);
         }
     }
 
@@ -51,8 +45,6 @@ public sealed class BenchJobSystem : UpdateSystem {
     public struct MoveJob : IJobParallelForTransform {
         [ReadOnly]
         public Vector3 moveVector;
-        public void Execute(int index, TransformAccess transform) {
-            transform.position += this.moveVector;
-        }
+        public void Execute(int index, TransformAccess transform) => transform.position += this.moveVector;
     }
 }
